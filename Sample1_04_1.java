d441fa59f5c24028220c21110e03fc221f1ae7fa
SK6595 <173334721+SK6595@users.noreply.github.com>
2025-03-29 05:49:59 +0000

    「変数の扱い、定数」学習完了

--- Sample1_04_1.java
+++ Sample1_04_1.java
@@ -5,11 +5,11 @@
 class Sample1_04_1 {
 	public static void main (String[] args) {
 		
-		int    teika   = 1000 ;             //定価の金額
+		final int LIST_PRICE = 1000 ;       //定価の金額
 		
-		double zeiritu = 1.08 ;             //消費税
+		double ZEIRITU = 1.08 ;             //消費税
 		
-		double siharai = teika * zeiritu ;  //支払い額を計算
+		double siharai = teika * ZEIRITU ;  //支払い額を計算
 		
 		System.out.println(siharai) ;       //支払い額を画面に表示
 		
