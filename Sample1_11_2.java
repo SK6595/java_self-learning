3086e530f4205c00deb467be8979fce1bbc27547
SK6595 <173334721+SK6595@users.noreply.github.com>
2025-06-21 06:50:24 +0000

    「繰り返しwhile」学習完了②

--- Sample1_11_2.java
+++ Sample1_11_2.java
@@ -1,7 +1,7 @@
-class Sample1_11_1 {
+class Sample1_11_2 {
 	public static void main (String[] args) {
 
-		//while文のしくみ
+		//do-while文のしくみ
 
 		/*コマンドライン引数で受け取った数字の累乗の数のうち、100未満のもののみを表示するプログラムを作る。
 		**【例】コマンドライン引数での入力：3 → 表示される数：3 , 9 , 27 , 81
@@ -10,13 +10,13 @@ class Sample1_11_1 {
 		int receiveNumber = Integer.parseInt(args[0]) ;  //ループ毎にcalcNumberに掛ける数（コマンドライン引数で受け取った値）
 		int calcNumber    = receiveNumber ;              //表示する数（初期値：コマンドライン引数で受け取った値）
 
-		while( calcNumber < 100 ){
+		do{
 
 			System.out.println("calcNumber：" + calcNumber);
 
 			calcNumber *= receiveNumber ;
 
-		}
+		}while( calcNumber < 100 );
 
 	}
 }
